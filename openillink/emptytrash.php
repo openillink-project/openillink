21de171d39443c5d24de302c9e487dff78d2d2a0
Jérôme Zbinden <jerome.zbinden@chuv.ch>
2019-03-05 06:56:37 +0100

    Admin: more rights needed to empty trash
    * Only allow admins and super-admins to empty trash

--- openillink/emptytrash.php
+++ openillink/emptytrash.php
@@ -3,7 +3,7 @@
 // ***************************************************************************
 // ***************************************************************************
 // This file is part of OpenILLink software.
-// Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2017 CHUV.
+// Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2017, 2019 CHUV.
 // Original author(s): Pablo Iriarte <pablo@iriarte.ch>
 // Other contributors are listed in the AUTHORS file at the top-level
 // directory of this distribution.
@@ -32,7 +32,7 @@ require_once ("includes/connexion.php");
 require_once ("includes/toolkit.php");
 
 if (!empty($_COOKIE['illinkid'])){
-    if (($monaut == "admin")||($monaut == "sadmin")||($monaut == "user")){
+    if (($monaut == "admin")||($monaut == "sadmin")){
         $action = "";
         $action= (!empty($_POST['action'])) && isValidInput($_POST['action'],9,'s',false,array('deleteall'))?$_POST['action']:'';
         $pagetitle = format_string(__("%institution_name orders: empty trash"), array('institution_name' => $configinstitution[$lang]));

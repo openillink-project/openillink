077860cc3b699ea37fad6bc9ba74496d328964d1
Jérôme Zbinden <jerome.zbinden@chuv.ch>
2024-06-14 08:59:34 +0200

    Orders table: updated DB doi+uid col length and index
    * Increased doi and uid columns to 255 chars
    * Replaced index on PMID, doi and uid with individual indexes
    * Added `anneevolpage` index on annee, volume and pages columns

--- openillink/data/openillink_structure.sql
+++ openillink/data/openillink_structure.sql
@@ -138,8 +138,8 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `isbn` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `issn` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `eissn` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `doi` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `uid` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `doi` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `remarques` text COLLATE utf8_unicode_ci,
   `remarquespub` text COLLATE utf8_unicode_ci,
   `historique` text COLLATE utf8_unicode_ci,
@@ -168,7 +168,10 @@ CREATE TABLE IF NOT EXISTS `orders` (
   KEY `sid` (`sid`,`pid`),
   KEY `stade` (`stade`),
   KEY `volume` (`volume`),
-  KEY `ui` (`uid`,`doi`,`PMID`),
+  KEY `uid` (`uid`),
+  KEY `doi` (`doi`),
+  KEY `pmid` (`PMID`),
+  KEY `anneevolpage` (`annee`,`volume`,`pages`),
   KEY `titre_periodique` (`titre_periodique`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
